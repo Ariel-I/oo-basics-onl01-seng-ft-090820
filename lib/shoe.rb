aba36008fdc70ed0e8cd454a8feaddc87550611c
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 05:55:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,5 +8,39 @@ class Shoe
     @brand 
   end 
   
+  def color=(shoe_color)
+    @color = shoe_color 
+  end 
+  
+  def color 
+    @color 
+  end 
+  
+  def size=(shoe_size)
+    @size = shoe_size
+  end 
+  
+  def size 
+    @size 
+  end 
+    
+  def materials=(shoe_materials)
+    @materials = shoe_materials
+  end 
+  
+  def materials
+    @materials
+  end 
+  
+  def condition=(shoe_condition)
+    @condition = shoe_condition
+  end 
+  
+  def condition
+    @condition
+  end 
+  
+  def cobble 
+  end 
   
 end 
